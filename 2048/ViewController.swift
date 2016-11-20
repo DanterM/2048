4426bf79075fa9b70c7bca5e404a51c6536928ef
DanterM <jarvis4m@gmail.com>
2016-11-20 23:21:25 +0800

    添加弹出提示框

--- 2048/ViewController.swift
+++ 2048/ViewController.swift
@@ -20,6 +20,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func startGame(_ sender: UIButton) {
+        // 弹出提示框
+        let alertVc = UIAlertController(title: "开始！", message: "游戏就要开始，你准备好了吗？", preferredStyle: UIAlertControllerStyle.alert)
+        let startAction = UIAlertAction(title: "Ready Go!", style: UIAlertActionStyle.default) {
+            (action: UIAlertAction!) -> Void in
+            // modal出标签控制器
+            self.present(MainTabBarViewController(), animated: true, completion: nil)
+        }
+        alertVc.addAction(startAction)
+        self.present(alertVc, animated: true, completion: nil)
+    }
 }
 
